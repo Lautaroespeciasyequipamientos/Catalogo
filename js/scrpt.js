4e82e6b86973cd40241aa6f4cd7444d91b6d1819
Lautaroespeciasyequipamientos <lautarocrismanich@gmail.com>
2026-08-30 21:45:57 -0300

    JS MENU

--- js/scrpt.js
+++ js/scrpt.js
@@ -41,3 +41,43 @@ function sendOrder() {
     }
 });
 }
+
+//COMPORTAMIENTO DE MENU
+const IconMenu = document.getElementById('iconMenu')
+const CntMenu = document.getElementById('cntMenu')
+
+const OptionsMenu = document.querySelectorAll('.optionMenu');
+
+const sections = document.querySelectorAll('.fade-up')
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) entry.target.classList.add('visible')
+  });
+}, { threshold: 0.3 })
+
+sections.forEach(sec => observer.observe(sec))
+
+IconMenu.addEventListener('click', () => {
+  IconMenu.classList.add('hiddenIcon')
+  IconMenu.classList.remove('visibleIcon')
+  
+  CntMenu.classList.add('visibleMenu')
+  CntMenu.classList.remove('hiddenMenu')
+})
+
+OptionsMenu
+
+OptionsMenu.forEach(function (opcion) {
+  opcion.addEventListener('click', function (event) {
+
+    IconMenu.classList.remove('hiddenIcon')
+    IconMenu.classList.add('visibleIcon')
+    
+    CntMenu.classList.remove('visibleMenu')
+    CntMenu.classList.add('hiddenMenu')
+
+  });
+
+
+});
